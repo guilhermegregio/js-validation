cb21278fa5cb539104b192999fe777e1c885f1f5
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2014-08-13 14:44:36 -0300

    added browserify

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
 	require('load-grunt-tasks')(grunt);
 	require('time-grunt')(grunt);
 
+	var remapify = require('remapify');
 
 	grunt.initConfig({
 		// Config Tasks
@@ -15,6 +16,25 @@ module.exports = function (grunt) {
 				singleRun: true
 			},
 			watch: {
+			}
+		},
+		browserify: {
+			options: {
+				bundleOptions: {
+					standalone: 'Validator',
+					debug: true
+				}
+			},
+			dist: {
+				src: ['./src/teste/index.js'],
+				dest: './build/js/Validator.js'
+			}
+		},
+		uglify: {
+			build: {
+				files: {
+					'build/js/Validator.min.js': ['build/js/Validator.js']
+				}
 			}
 		}
 	});
